b48056224d90822b320dd8f0f66684e3904e9bec
parkkhee <khp963@naver.com>
2023-05-18 14:50:08 +0900

    스프링 시큐리티가 자격없는 요청에 대해서 자의적으로 정한 응답 양식을 커스터마이징

---
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/ApiV1MemberController.java
@@ -0,0 +1,78 @@
+package com.ll.rest_api.boundedContext.member.controller;
+
+import com.ll.rest_api.base.rsData.RsData;
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import com.ll.rest_api.boundedContext.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.ALL_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@Tag(name = "ApiV1MemberController", description = "로그인, 로그인 된 회원의 정보")
+public class ApiV1MemberController {
+    private final MemberService memberService;
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class LoginResponse {
+        private final String accessToken;
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인, 엑세스 토큰 발급")
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
+
+//        resp.addHeader("Authentication", accessToken);
+
+        return RsData.of(
+                "S-1",
+                "엑세스토큰이 생성되었습니다.",
+                new LoginResponse(accessToken)
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MeResponse {
+        private final Member member;
+    }
+
+    // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다.
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    @Operation(summary = "로그인된 사용자의 정보", security = @SecurityRequirement(name = "bearerAuth"))
+    public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
+        Member member = memberService.findByUsername(user.getUsername()).get();
+
+        return RsData.of(
+                "S-1",
+                "성공",
+                new MeResponse(member)
+        );
+    }
+
+
+}
---
+++ src/main/java/com/ll/rest_api/boundedContext/member/service/MemberService.java
@@ -0,0 +1,50 @@
+package com.ll.rest_api.boundedContext.member.service;
+
+import com.ll.rest_api.base.jwt.JwtProvider;
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import com.ll.rest_api.boundedContext.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+
+    public Member join(String username, String password, String email) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public String genAccessToken(String username, String password) {
+        Member member = findByUsername(username).orElse(null);
+
+        if (member == null) return null;
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            return null;
+        }
+
+        return jwtProvider.genToken(member.toClaims(), 60 * 60 * 24 * 365);
+    }
+
+    public Optional<Member> findById(Long id) {
+        return memberRepository.findById(id);
+    }
+}
